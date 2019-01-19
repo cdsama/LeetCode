f15f81c58fd3df6a0e1eeb62670ee3bed759f1ee
cdsama <cdsama@gmail.com>
2019-01-19 17:54:37 +0800

    add_problem(0136  Easy   single-number)

--- src/0136.cpp
+++ src/0136.cpp
@@ -4,20 +4,22 @@
 136. Single Number
 Easy
 
-Given a non-empty array of integers, every element appears twice except for one. Find that single one.
-Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
-Example 1:
+Given a non-empty array of integers, every element appears twice except for one.
+Find that single one.
 
-Input: [2,2,1]
-Output: 1
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it
+without using extra memory?
+
+Example 1:
+    Input: [2,2,1]
+    Output: 1
 
 Example 2:
+    Input: [4,1,2,1,2]
+    Output: 4
 
-Input: [4,1,2,1,2]
-Output: 4
-
-Tags: 
+Tags:
     1. Hash Table
     2. Bit Manipulation
 
@@ -31,14 +33,21 @@ Similar Questions:
 */
 
 class Solution {
-public:
+   public:
     int singleNumber(vector<int>& nums) {
-        
+        int result = 0;
+        for (const auto i : nums) {
+            result ^= i;
+        }
+        return result;
     }
 };
 
 TEST_CASE("single-number", "[136][Easy][hash-table][bit-manipulation]") {
-    //TODO
-    CHECK(true);
+    vector<pair<vector<int>, int>> tests = {
+        {{2, 2, 1}, 1}, {{4, 1, 2, 1, 2}, 4}, {{1, 2, 3, 4, 5, 5, 4, 3, 1}, 2}};
+    Solution s;
+    for (auto&& p : tests) {
+        CHECK(s.singleNumber(p.first) == p.second);
+    }
 }
-
