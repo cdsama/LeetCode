40e2830b599f259db32042791a5fbb88186c0088
cdsama <cdsama@gmail.com>
2019-01-21 23:26:37 +0800

    add new problems

---
+++ src/0977.cpp
@@ -0,0 +1,43 @@
+#include "LeetCode.hpp"
+
+/*
+977. Squares of a Sorted Array
+Easy
+
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+Note:
+
+1 <= A.length <= 10000
+-10000 <= A[i] <= 10000
+A is sorted in non-decreasing order.
+
+Tags: 
+    1. Array
+    2. Two Pointers
+    3. Sort
+
+*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        
+    }
+};
+
+TEST_CASE("squares-of-a-sorted-array", "[977][Easy][array][two-pointers][sort]") {
+    //TODO
+    CHECK(true);
+}
+
