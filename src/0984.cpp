d133c15786cd63dc761d03b9374fcf068e47dfd1
chendi <cdsama@gmail.com>
2019-01-28 21:59:32 +0800

    # add_problem(0981 Medium time-based-key-value-store) # Time Based Key-Value Store
    # add_problem(0982  Hard  triples-with-bitwise-and-equal-to-zero) # Triples with Bitwise AND Equal To Zero
    
    # add_problem(0983 Medium minimum-cost-for-tickets) # Minimum Cost For Tickets
    
    # add_problem(0984  Easy  string-without-aaa-or-bbb) # String Without AAA or BBB

---
+++ src/0984.cpp
@@ -0,0 +1,46 @@
+#include "LeetCode.hpp"
+
+/*
+984. String Without AAA or BBB
+Easy
+
+Given two integers A and B, return any string S such that:
+
+S has length A + B and contains exactly A 'a' letters, and exactly B 'b' letters;
+The substring 'aaa' does not occur in S;
+The substring 'bbb' does not occur in S.
+
+Example 1:
+
+Input: A = 1, B = 2
+Output: "abb"
+Explanation: "abb", "bab" and "bba" are all correct answers.
+
+Example 2:
+
+Input: A = 4, B = 1
+Output: "aabaa"
+
+Note:
+
+0 <= A <= 100
+0 <= B <= 100
+It is guaranteed such an S exists for the given A and B.
+
+Tags: 
+    1. Greedy
+
+*/
+
+class Solution {
+public:
+    string strWithout3a3b(int A, int B) {
+        
+    }
+};
+
+TEST_CASE("string-without-aaa-or-bbb", "[984][Easy][greedy]") {
+    //TODO
+    CHECK(true);
+}
+
