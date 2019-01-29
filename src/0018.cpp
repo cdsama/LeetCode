b6aa0604234768b82ac1a29dcf02a458856b52bc
cdsama <cdsama@gmail.com>
2019-01-29 18:41:50 +0800

    [skip ci] add_problem(0018 Medium 4sum) WA tbc

--- src/0018.cpp
+++ src/0018.cpp
@@ -4,10 +4,10 @@
 18. 4Sum
 Medium
 
-Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
-Note:
-The solution set must not contain duplicate quadruplets.
-Example:
+Given an array nums of n integers and an integer target, are there elements a,
+b, c, and d in nums such that a + b + c + d = target? Find all unique
+quadruplets in the array which gives the sum of target. Note: The solution set
+must not contain duplicate quadruplets. Example:
 
 Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
 
@@ -18,7 +18,7 @@ A solution set is:
   [-2,  0, 0, 2]
 ]
 
-Tags: 
+Tags:
     1. Array
     2. Hash Table
     3. Two Pointers
@@ -31,14 +31,47 @@ Similar Questions:
 */
 
 class Solution {
-public:
+   public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+        vector<vector<int>> ret;
+        if (nums.size() < 4) {
+            return ret;
+        }
+        sort(nums.begin(), nums.end());
+        size_t count = nums.size();
+        std::unordered_map<int, set<pair<size_t, size_t>>> cache;
+        for (size_t i = 0; i < count - 1; ++i) {
+            for (size_t j = i + 1; j < count; ++j) {
+                cache[target - nums[i] - nums[j]].insert({i, j});
+            }
+        }
+
+        for (size_t i = 0; i < count - 1; ++i) {
+            for (size_t j = i + 1; j < count; ++j) {
+                int temp = nums[i] + nums[j];
+                auto& res = cache[temp];
+                for (auto itr = res.begin(); itr != res.end();) {
+                    if (itr->first != i && itr->first != j &&
+                        itr->second != i && itr->second != j) {
+                        ret.push_back({nums[i], nums[j], nums[itr->first],
+                                       nums[itr->second]});
+                        itr = res.erase(itr);
+                        cache[target - temp].erase({i, j});
+                    } else {
+                        ++itr;
+                    }
+                }
+            }
+        }
+
+        return ret;
     }
 };
 
 TEST_CASE("4sum", "[18][Medium][array][hash-table][two-pointers]") {
-    //TODO
-    CHECK(true);
+    Solution s;
+    vector<vector<int>> key = {{-1, 0, 0, 1}, {-2, -1, 1, 2}, {-2, 0, 0, 2}};
+    vector<int> nums = {1, 0, -1, 0, -2, 2};
+    vector<vector<int>> ans = s.fourSum(nums, 0);
+    CHECK(key == ans);
 }
-
