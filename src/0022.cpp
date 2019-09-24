95b7daf790148630fcbfd4e69cd5bb0816705809
cdsama <cdsama@gmail.com>
2019-09-24 20:30:34 +0800

    add_problem(0022 Medium generate-parentheses)

--- src/0022.cpp
+++ src/0022.cpp
@@ -4,7 +4,8 @@
 22. Generate Parentheses
 Medium
 
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Given n pairs of parentheses, write a function to generate all combinations of
+well-formed parentheses.
 
 For example, given n = 3, a solution set is:
 
@@ -16,7 +17,7 @@ For example, given n = 3, a solution set is:
   "()()()"
 ]
 
-Tags: 
+Tags:
     1. String
     2. Backtracking
 
@@ -27,14 +28,34 @@ Similar Questions:
 */
 
 class Solution {
-public:
+   public:
     vector<string> generateParenthesis(int n) {
-        
+        vector<string> result;
+        addParenthesis(result, "", n, 0);
+        return result;
+    }
+
+    void addParenthesis(vector<string>& result, string&& s, int left,
+                        int right) {
+        if (left == 0 && right == 0) {
+            result.push_back(s);
+            return;
+        }
+        if (left > 0) {
+            addParenthesis(result, s + "(", left - 1, right + 1);
+        }
+        if (right > 0) {
+            addParenthesis(result, s + ")", left, right - 1);
+        }
     }
 };
 
 TEST_CASE("generate-parentheses", "[22][Medium][string][backtracking]") {
-    //TODO
-    CHECK(true);
+    Solution s;
+    auto result = s.generateParenthesis(3);
+    decltype(result) answer = {"((()))", "(()())", "(())()", "()(())",
+                               "()()()"};
+    std::sort(result.begin(), result.end());
+    std::sort(answer.begin(), answer.end());
+    CHECK(result == answer);
 }
-
